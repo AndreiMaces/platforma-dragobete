12fb345028fdb0f337792e8982d8393b6e5ead4f
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2023-02-15 00:57:40 +0200

    Added design

--- app.js
+++ app.js
@@ -6,25 +6,29 @@ const ejs = require("ejs");
 const { default: mongoose } = require("mongoose");
 const PORT = process.env.PORT;
 
-const modelMesaj = mongoose.model("Mesaj", {text: String});
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const modelMesaj = mongoose.model("Mesaj", { text: String });
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
-})
+  res.render("index");
+});
 
 app.post("/", (req, res) => {
-    if(req.body.text.length > 5000 || req.body.text.length < 1) return res.render("error");
-    const mesaj = new modelMesaj({text: req.body.text});
-    mesaj.save();
-    res.render("success");
-})
+  if (req.body.text.length > 5000 || req.body.text.length < 1)
+    return res.render("error");
+  const mesaj = new modelMesaj({ text: req.body.text });
+  mesaj.save();
+  res.render("success");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
