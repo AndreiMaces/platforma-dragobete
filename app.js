026d8f9c0be800644bac4bdd34f85c0585a2327c
MacesAndrei <andrei.maces@cst.ro>
2023-02-14 16:37:25 +0200

    Add post route && validation

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const { default: mongoose } = require("mongoose");
+const PORT = process.env.PORT;
+
+const modelMesaj = mongoose.model("Mesaj", {text: String});
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.post("/", (req, res) => {
+    if(req.body.text.length > 5000 || req.body.text.length < 1) return res.render("error");
+    const mesaj = new modelMesaj({text: req.body.text});
+    mesaj.save();
+    res.render("success");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
