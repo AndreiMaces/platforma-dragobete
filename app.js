8496c0780d3998eb23f3b4152f317c8fd901958c
AndreiMaces <80191286+AndreiMaces@users.noreply.github.com>
2023-02-17 19:03:35 +0200

    Add persoana field && ejs-mate layout

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const ejsMate = require("ejs-mate");
 const ejs = require("ejs");
 const { default: mongoose } = require("mongoose");
 const PORT = process.env.PORT;
 
-const modelMesaj = mongoose.model("Mesaj", { text: String });
+const modelMesaj = mongoose.model("Mesaj", { persoana: String, text: String });
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -16,15 +17,48 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    persoana: "",
+    mesaj: "",
+    error: "",
+  });
 });
 
-app.post("/", (req, res) => {
-  if (req.body.text.length > 5000 || req.body.text.length < 1)
-    return res.render("error");
-  const mesaj = new modelMesaj({ text: req.body.text });
+app.get("/admin", async (req, res) => {
+  if (req.query.password !== "Astronaut123") {
+    res.redirect("/");
+  }
+  const raspunsuri = await modelMesaj.find({});
+  console.log(raspunsuri);
+  res.render("admin", { raspunsuri });
+});
+
+app.delete("/admin", async (req, res) => {
+  await modelMesaj.deleteMany({});
+  res.redirect("/admin");
+});
+
+app.post("/", async (req, res) => {
+  if (!req.body.persoana)
+    return res.render("index", {
+      persoana: "",
+      mesaj: req.body.text ?? "",
+      error: "Numele persoanei trebuie introdus!",
+    });
+  if (req.body.text.length < 1 || req.body.text.length > 5000)
+    return res.render("index", {
+      persoana: req.body.persoana ?? "",
+      mesaj: "",
+      error: "Mesajul trebuie sa contina maxim 5000 de caractere",
+    });
+
+  const mesaj = await new modelMesaj({
+    persoana: req.body.persoana,
+    text: req.body.text,
+  });
   mesaj.save();
   res.render("success");
 });
